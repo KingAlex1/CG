38f5d3ab7973f0ce16664efbfe11c07035193f22
Alex Bogomolov <alex-sert2015@mail.ru>
2018-10-27 01:52:09 +0300

    Added some featers

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,12 +1,27 @@
 import 'normalize.css';
 import './index.scss';
+import './about.scss';
+import './animation.scss';
+import  './form.scss';
 
 console.log('index.js');
-let vimeo_player = document.querySelector('#vimeo_player');
-let video_height = vimeo_player.clientHeight ;
-let overlay_height = document.querySelector('#overlay_background').clientHeight;
-let xx = vimeo_player.style.marginTop = video_height - overlay_height + 'px' ;
-console.log(xx);
+// let vimeo_player = document.querySelector('#vimeo_player');
+// let video_height = vimeo_player.clientHeight;
+// let overlay_height = document.querySelector('#overlay_background').clientHeight;
+// let xx = vimeo_player.style.marginTop = video_height - overlay_height + 'px';
+// console.log(xx);
 
+let arrow = document.getElementById('header__arrow');
+let i = arrow.offsetTop;
+let endPosition = i + 12;
 
+function moveBlock() {
+  arrow.style.top = i + 'px';
+  i = i + 0.2;
+  if (i > endPosition) {
+    i = i - 10;
+    arrow.style.top = i + 'px';
+  }
+}
 
+setInterval(moveBlock, 40);
